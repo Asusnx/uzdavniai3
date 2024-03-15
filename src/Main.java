5d9f7431a6f484dce8382ddacdb421435188d957
Asusnx <asus0111@gmail.com>
2024-03-15 15:43:57 +0200

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,33 @@
+public class Main {
+    public static void main(String[] args) {
+
+//        1 uzdiotis
+
+        for (int x = 0; x <10 ; x++) {
+            System.out.println("labas");
+        }
+
+//        2 uzduotis
+
+        for (int x = 0; x <10 ; x++) {
+            System.out.println(x);
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+}
